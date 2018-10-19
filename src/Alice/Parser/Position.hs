0046ece3edba37277c8827fbf5942376c008cc54
Makarius <makarius@sketis.net>
2018-10-19 21:19:23 +0200

    more detailed SourcePos

--- src/Alice/Parser/Position.hs
+++ src/Alice/Parser/Position.hs
@@ -6,7 +6,7 @@ Token source positions.
 
 
 module Alice.Parser.Position
-  ( SourcePos (EOF, sourceFile, sourceLine, sourceColumn),
+  ( SourcePos (EOF, sourceFile, sourceLine, sourceColumn, sourceOffset),
     SourceName,
     initialPos,
     advancePos,
@@ -16,35 +16,46 @@ module Alice.Parser.Position
 type SourceName = String
 type Line = Int
 type Column = Int
+type Offset = Int
 
 data SourcePos = SourcePos { sourceFile   :: SourceName,
                              sourceLine   :: Line,
-                             sourceColumn :: Column}
+                             sourceColumn :: Column,
+                             sourceOffset :: Offset }
                | EOF
                deriving (Eq, Ord)
 
 
 initialPos :: SourceName -> SourcePos
-initialPos name = SourcePos name 1 1
+initialPos name = SourcePos name 1 1 1
 
 advanceLine line c = if c == '\n' then line + 1 else line
 advanceColumn column c = if c == '\n' then 1 else column + 1
+advanceOffset offset c = offset + 1
 
 advancePos :: SourcePos -> Char -> SourcePos
-advancePos (SourcePos name line column) c =
-  SourcePos name (advanceLine line c) (advanceColumn column c)
+advancePos (SourcePos name line column offset) c =
+  SourcePos name
+    (advanceLine line c)
+    (advanceColumn column c)
+    (advanceOffset offset c)
 
 advancesPos :: SourcePos -> String -> SourcePos
-advancesPos (SourcePos name line column) s =
-  SourcePos name (foldl advanceLine line s) (foldl advanceColumn column s)
+advancesPos (SourcePos name line column offset) s =
+  SourcePos name
+    (foldl advanceLine line s)
+    (foldl advanceColumn column s)
+    (foldl advanceOffset offset s)
 
 
 instance Show SourcePos where
   show EOF = "(end of input)"
-  show (SourcePos name line column)
+  show (SourcePos name line column offset)
     | null name = showLineColumn
     | otherwise = "\"" ++ name ++ "\" " ++ showLineColumn
     where
-      showLineColumn    = "(line " ++ show line ++
-                          ", column " ++ show column ++
-                          ")"
+      showLineColumn =
+        "(line " ++ show line ++
+        ", column " ++ show column ++
+        ", offset " ++ show offset ++
+        ")"
