9abf6f0412802685c0f7acd0e0bf32b8c26ddeee
Makarius <makarius@sketis.net>
2018-10-26 17:34:31 +0200

    clarified SourcePos -- allow undefined position

--- src/Alice/Parser/Position.hs
+++ src/Alice/Parser/Position.hs
@@ -8,30 +8,35 @@ Token source positions.
 module Alice.Parser.Position
   ( SourcePos (EOF, sourceFile, sourceLine, sourceColumn, sourceOffset),
     SourceName,
+    noPos,
     initialPos,
     advancePos,
     advancesPos )
   where
 
+import Data.List
+
 type SourceName = String
-type Line = Int
-type Column = Int
-type Offset = Int
 
-data SourcePos = SourcePos { sourceFile   :: SourceName,
-                             sourceLine   :: Line,
-                             sourceColumn :: Column,
-                             sourceOffset :: Offset }
-               | EOF
-               deriving (Eq, Ord)
+data SourcePos =
+  SourcePos {
+    sourceFile :: SourceName,
+    sourceLine :: Int,
+    sourceColumn :: Int,
+    sourceOffset :: Int }
+  | EOF
+  deriving (Eq, Ord)
 
+
+noPos :: SourcePos
+noPos = SourcePos "" 0 0 0
 
 initialPos :: SourceName -> SourcePos
 initialPos name = SourcePos name 1 1 1
 
-advanceLine line c = if c == '\n' then line + 1 else line
-advanceColumn column c = if c == '\n' then 1 else column + 1
-advanceOffset offset c = offset + 1
+advanceLine line c = if line <= 0 || c /= '\n' then line else line + 1
+advanceColumn column c = if column <= 0 then column else if c == '\n' then 1 else column + 1
+advanceOffset offset c = if offset <= 0 then offset else offset + 1
 
 advancePos :: SourcePos -> Char -> SourcePos
 advancePos (SourcePos name line column offset) c =
@@ -50,12 +55,15 @@ advancesPos (SourcePos name line column offset) s =
 
 instance Show SourcePos where
   show EOF = "(end of input)"
-  show (SourcePos name line column offset)
-    | null name = showDetails
-    | otherwise = "\"" ++ name ++ "\" " ++ showDetails
+  show (SourcePos name line column offset) =
+    if null showName then showDetails
+    else if null showDetails then showName
+    else showName ++ " " ++ showDetails
     where
+      detail a i = if i <= 0 then "" else a ++ " " ++ show i
+      details = [detail "line" line, detail "column" column, detail "offset" offset]
       showDetails =
-        "(line " ++ show line ++
-        ", column " ++ show column ++
-        ", offset " ++ show offset ++
-        ")"
+        case filter (not . null) details of
+          [] -> ""
+          ds -> "(" ++ intercalate "," ds ++ ")"
+      showName = if null name then "" else "\"" ++ name ++ "\""
