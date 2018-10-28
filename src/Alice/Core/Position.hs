00dc13e157ec5dbe04ad36a879fcef38f902b256
Makarius <makarius@sketis.net>
2018-10-28 17:06:23 +0100

    maintain position range as in Isabelle

--- src/Alice/Core/Position.hs
+++ src/Alice/Core/Position.hs
@@ -6,12 +6,16 @@ Token source positions.
 
 
 module Alice.Core.Position
-  ( SourcePos (EOF, sourceFile, sourceLine, sourceColumn, sourceOffset),
+  ( SourcePos (EOF, sourceFile, sourceLine, sourceColumn, sourceOffset, sourceEndOffset),
+    SourceRange,
     noPos,
     fileOnlyPos,
     filePos,
     advancePos,
     advancesPos,
+    noRangePos,
+    rangePos,
+    range,
     posProperties)
   where
 
@@ -22,49 +26,64 @@ data SourcePos =
     sourceFile :: String,
     sourceLine :: Int,
     sourceColumn :: Int,
-    sourceOffset :: Int }
+    sourceOffset :: Int,
+    sourceEndOffset :: Int }
   | EOF
   deriving (Eq, Ord)
 
+type SourceRange = (SourcePos, SourcePos)
 
 noPos :: SourcePos
-noPos = SourcePos "" 0 0 0
+noPos = SourcePos "" 0 0 0 0
 
 fileOnlyPos :: String -> SourcePos
-fileOnlyPos file = SourcePos file 0 0 0
+fileOnlyPos file = SourcePos file 0 0 0 0
 
 filePos :: String -> SourcePos
-filePos file = SourcePos file 1 1 1
+filePos file = SourcePos file 1 1 1 0
 
 advanceLine line c = if line <= 0 || c /= '\n' then line else line + 1
 advanceColumn column c = if column <= 0 then column else if c == '\n' then 1 else column + 1
 advanceOffset offset c = if offset <= 0 then offset else offset + 1
 
 advancePos :: SourcePos -> Char -> SourcePos
-advancePos (SourcePos file line column offset) c =
+advancePos (SourcePos file line column offset endOffset) c =
   SourcePos file
     (advanceLine line c)
     (advanceColumn column c)
     (advanceOffset offset c)
+    endOffset
 
 advancesPos :: SourcePos -> String -> SourcePos
-advancesPos (SourcePos file line column offset) s =
+advancesPos (SourcePos file line column offset endOffset) s =
   SourcePos file
     (foldl advanceLine line s)
     (foldl advanceColumn column s)
     (foldl advanceOffset offset s)
+    endOffset
 
+noRangePos :: SourcePos -> SourcePos
+noRangePos (SourcePos file line column offset _) =
+  SourcePos file line column offset 0
+
+rangePos :: SourceRange -> SourcePos
+rangePos (SourcePos file line column offset _, SourcePos _ _ _ offset' _) =
+  SourcePos file line column offset offset'
+
+range :: (SourcePos, SourcePos) -> SourceRange
+range (pos, pos') = (rangePos (pos, pos'), noRangePos pos')
 
 posProperties :: SourcePos -> [(String, String)]
-posProperties (SourcePos file line column offset) =
+posProperties (SourcePos file line column offset endOffset) =
   (if null file then [] else [("file", file)]) ++
   (if line <= 0 then [] else [("line", show line)]) ++
   (if column <= 0 then [] else [("column", show column)]) ++
-  (if offset <= 0 then [] else [("offset", show offset)])
+  (if offset <= 0 then [] else [("offset", show offset)]) ++
+  (if endOffset <= 0 then [] else [("end_offset", show endOffset)])
 
 instance Show SourcePos where
   show EOF = "(end of input)"
-  show (SourcePos file line column offset) =
+  show (SourcePos file line column _ _) =
     if null showName then showDetails
     else if null showDetails then showName
     else showName ++ " " ++ showDetails
