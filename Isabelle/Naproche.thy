eb3a1a0c1a8e48912305fafcf7441f7990fa6faa
Makarius <makarius@sketis.net>
2018-11-07 23:49:31 +0100

    clarified

--- Isabelle/Naproche.thy
+++ Isabelle/Naproche.thy
@@ -5,23 +5,10 @@ Support for NaProChe / ForTheL within Isabelle.
 *)
 
 theory Naproche
-  imports Pure Haskell.Haskell
+  imports Pure
   keywords "forthel_file" :: thy_load
 begin
 
-
-section \<open>Commands\<close>
-
 ML_file "naproche.ML"
-
-ML \<open>
-val _ =
-  Outer_Syntax.command \<^command_keyword>\<open>forthel_file\<close> "check NaProChe .ftl file"
-    (Parse.token Parse.path >> (fn tok =>
-      Toplevel.keep (fn st =>
-        (case Token.get_files tok of
-          [Exn.Res file] => Naproche.forthel_file (Toplevel.context_of st) file
-        | _ => ()))));
-\<close>
 
 end
