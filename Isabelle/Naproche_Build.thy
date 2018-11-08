72527bf89b5292f2cf3ae698978e4a494318d3f6
Makarius <makarius@sketis.net>
2018-11-08 14:24:19 +0100

    tuned

--- Isabelle/Naproche_Build.thy
+++ Isabelle/Naproche_Build.thy
@@ -28,18 +28,14 @@ ML_command \<open>
     let
       val master_dir = Resources.master_directory \<^theory>;
       val sources = [\<^path>\<open>file_format.scala\<close>];
-      val jar = Path.append master_dir \<^path>\<open>naproche.jar\<close>;
+      val target = \<^path>\<open>naproche.jar\<close>;
       val (out, rc) =
         Isabelle_System.bash_output
-          (cat_lines
-           ["set -e",
-            "cd " ^ File.bash_path master_dir,
-            "cp -p -f " ^ File.bash_paths sources ^ " " ^ File.bash_path tmp_dir,
-            "cd " ^ File.bash_path tmp_dir,
-            "mkdir classes",
-            "isabelle scalac -d classes $ISABELLE_SCALAC_OPTIONS " ^ File.bash_paths sources,
-            "cd classes",
-            "isabelle_jdk jar cf " ^ Bash.string (File.platform_path jar) ^ " isabelle"]);
+          ("cd " ^ File.bash_path tmp_dir ^ " && \n\
+           \isabelle scalac $ISABELLE_SCALAC_OPTIONS " ^
+              File.bash_paths (map (Path.append master_dir) sources) ^ " && \n\
+           \isabelle_jdk jar cf " ^
+              Bash.string (File.platform_path (Path.append master_dir target)) ^ " *");
     in if rc = 0 then writeln out else error out end);
 \<close>
 
