f9699112f63069dde70c64b2cf823c6f49479ed0
Steffen Frerix <steffenfrerix@gmx.de>
2018-11-12 13:40:18 +0100

    cleaned whitespace

--- src/Alice/ForTheL/Base.hs
+++ src/Alice/ForTheL/Base.hs
@@ -46,25 +46,31 @@ data FState = FState {
 
 
 
-initFS  = FState  eq [] nt sn
-                  cf rf [] []
-                  [] [] [] sp
-                  [] [] [] 0 False 0
+initFS = FState
+  eq [] nt sn
+  cf rf [] []
+  [] [] [] sp
+  [] [] [] 0 False 0
   where
-    eq  = [ ([Wd ["equal"], Wd ["to"], Vr], zTrm (-1) "="),
-            ([Wd ["nonequal"], Wd ["to"], Vr], Not . zTrm (-1) "=") ]
-    sp  = [ ([Sm "="], zTrm (-1) "="),
-            ([Sm "!", Sm "="], Not . zTrm (-1) "="),
-            ([Sm "-", Sm "<", Sm "-"], zTrm (-2) "iLess"),
-            ([Sm "-~-"], \(m:n:_) -> zAll "" $ Iff (zElem (zVar "") m) (zElem (zVar "") n)) ]
-    sn  = [ ([Sm "=", Vr], zTrm (-1) "=") ]
-    nt  = [ ([Wd ["function","functions"], Nm], zFun . head),
-            ([Wd ["set","sets"], Nm], zSet . head),
-            ([Wd ["element", "elements"], Nm, Wd ["of"], Vr], \(x:m:_) -> zElem x m),
-            ([Wd ["object", "objects"], Nm], zObj . head)]
-    rf  = [ ([Sm "[", Vr, Sm "]"], \(f:x:_) -> zApp f x)]
-    cf  = [ ([Sm "Dom", Sm "(",Vr,Sm ")"], zDom . head),
-            ([Sm "(", Vr, Sm ",", Vr, Sm ")"], \(x:y:_) -> zPair x y) ]
+    eq = [
+      ([Wd ["equal"], Wd ["to"], Vr], zTrm (-1) "="),
+      ([Wd ["nonequal"], Wd ["to"], Vr], Not . zTrm (-1) "=") ]
+    sp = [ 
+      ([Sm "="], zTrm (-1) "="),
+      ([Sm "!", Sm "="], Not . zTrm (-1) "="),
+      ([Sm "-", Sm "<", Sm "-"], zTrm (-2) "iLess"),
+      ([Sm "-~-"], \(m:n:_) -> zAll "" $
+        Iff (zElem (zVar "") m) (zElem (zVar "") n)) ]
+    sn = [ ([Sm "=", Vr], zTrm (-1) "=") ]
+    nt = [
+      ([Wd ["function","functions"], Nm], zFun . head),
+      ([Wd ["set","sets"], Nm], zSet . head),
+      ([Wd ["element", "elements"], Nm, Wd ["of"], Vr], \(x:m:_) -> zElem x m),
+      ([Wd ["object", "objects"], Nm], zObj . head)]
+    rf = [ ([Sm "[", Vr, Sm "]"], \(f:x:_) -> zApp f x)]
+    cf = [
+      ([Sm "Dom", Sm "(",Vr,Sm ")"], zDom . head),
+      ([Sm "(", Vr, Sm ",", Vr, Sm ")"], \(x:y:_) -> zPair x y) ]
 
 
 
@@ -113,8 +119,9 @@ prim_m_un_adj = getExpr (filter (unary . fst) . adj_expr) . prim_ml_prd
     unary (_  : pt) = unary pt
     unary _         = True
 
-prim_ml_prd p (pt, fm)  = do  (q, ts) <- ml_patt p pt
-                              return (q, fm $ zHole:zSlot:ts)
+prim_ml_prd p (pt, fm) = do 
+  (q, ts) <- ml_patt p pt
+  return (q, fm $ zHole:zSlot:ts)
 
 
 -- Notions and functions
@@ -123,40 +130,44 @@ prim_ntn, prim_of_ntn :: FTL UTerm -> FTL MNotion
 
 prim_ntn p  = getExpr ntn_expr ntn
   where
-    ntn (pt, fm)  = do  (q, vs, ts) <- nt_patt p pt
-                        return (q, fm $ zHole:ts, vs)
+    ntn (pt, fm) = do
+      (q, vs, ts) <- nt_patt p pt
+      return (q, fm $ zHole:ts, vs)
 
 prim_of_ntn p = getExpr ntn_expr ntn
   where
-    ntn (pt, fm)  = do  (q, vs, ts) <- of_patt p pt
-                        let fn v = fm $ (zVar v):zHole:ts
-                        return (q, foldr1 And $ map fn vs, vs)
+    ntn (pt, fm) = do 
+      (q, vs, ts) <- of_patt p pt
+      let fn v = fm $ (zVar v):zHole:ts
+      return (q, foldr1 And $ map fn vs, vs)
 
 prim_cm_ntn :: FTL UTerm -> FTL MTerm -> FTL MNotion
 prim_cm_ntn p s = getExpr ntn_expr ntn
   where
-    ntn (pt, fm)  = do  (q, vs, as, ts) <- cm_patt p s pt
-                        let fn v = fm $ zHole:v:ts
-                        return (q, foldr1 And $ map fn as, vs)
+    ntn (pt, fm) = do
+      (q, vs, as, ts) <- cm_patt p s pt
+      let fn v = fm $ zHole:v:ts
+      return (q, foldr1 And $ map fn as, vs)
 
 prim_fun :: FTL UTerm -> FTL UTerm
 prim_fun  = (>>= fun) . prim_ntn
   where
-    fun (q, Trm {trName = "=", trArgs = [_, t]}, _) | not (occursH t) = return (q, t)
+    fun (q, Trm {trName = "=", trArgs = [_, t]}, _)
+      | not (occursH t) = return (q, t)
     fun _ = mzero
 
 
 -- Symbolic primitives
 
-prim_cpr  = getExpr cpr_expr . prim_csm     -- T ... T
-prim_rpr  = getExpr rpr_expr . prim_rsm     -- v ... T
-prim_lpr  = getExpr lpr_expr . prim_lsm     -- T ... v
-prim_ipr  = getExpr ipr_expr . prim_ism     -- v ... v
+prim_cpr = getExpr cpr_expr . prim_csm
+prim_rpr = getExpr rpr_expr . prim_rsm
+prim_lpr = getExpr lpr_expr . prim_lsm
+prim_ipr = getExpr ipr_expr . prim_ism
 
-prim_cfn  = getExpr cfn_expr . prim_csm
-prim_rfn  = getExpr rfn_expr . prim_rsm
-prim_lfn  = getExpr lfn_expr . prim_lsm
-prim_ifn  = getExpr ifn_expr . prim_ism
+prim_cfn = getExpr cfn_expr . prim_csm
+prim_rfn = getExpr rfn_expr . prim_rsm
+prim_lfn = getExpr lfn_expr . prim_lsm
+prim_ifn = getExpr ifn_expr . prim_ism
 
 prim_csm p (pt, fm) = sm_patt p pt >>= \l -> return $ fm l
 prim_rsm p (pt, fm) = sm_patt p pt >>= \l -> return $ \t -> fm $ t:l
@@ -186,11 +197,12 @@ samePat _ _ = False
 -- most basic pattern parser: simply follow the pattern anf parse terms with p
 -- at variable places
 wd_patt p (Wd l : ls) = wd_tokenOf l >> wd_patt p ls
-wd_patt p (Vr : ls)   = do  (r, t) <- p
-                            (q, ts) <- wd_patt p ls
-                            return (r . q, t:ts)
-wd_patt _ []          = return (id, [])
-wd_patt _ _           = mzero
+wd_patt p (Vr : ls) = do
+  (r, t) <- p
+  (q, ts) <- wd_patt p ls
+  return (r . q, t:ts)
+wd_patt _ [] = return (id, [])
+wd_patt _ _  = mzero
 
 -- parses a symbolic pattern
 sm_patt p (Vr : ls)   = liftM2 (:) p $ sm_patt p ls
@@ -200,8 +212,8 @@ sm_patt _ _           = mzero
 
 -- parses a multi-subject pattern: follow the pattern, but ignore the wd_token
 -- right before the first variable. Then check that all "and" tokens have been
--- consumed. Example pattern: [Wd ["commute","commutes"], Wd ["with"], Vr]. Then we can parse
--- "a commutes with c and d" as well as "a and b commute".
+-- consumed. Example pattern: [Wd ["commute","commutes"], Wd ["with"], Vr]. Then
+-- we can parse "a commutes with c and d" as well as "a and b commute".
 ml_patt p (Wd l :_: Vr : ls)
                       = wd_tokenOf l >> na_patt p ls
 ml_patt p (Wd l : ls) = wd_tokenOf l >> ml_patt p ls
@@ -211,35 +223,36 @@ ml_patt _ _           = mzero
 -- parses a notion: follow the pattern to the name place, record names,
 -- then keep following the pattern
 nt_patt p (Wd l : ls) = wd_tokenOf l >> nt_patt p ls
-nt_patt p (Nm : ls)   = do  vs <- namlist
-                            (q, ts) <- wd_patt p ls
-                            return (q, vs, ts)
+nt_patt p (Nm : ls)   = do
+  vs <- namlist
+  (q, ts) <- wd_patt p ls
+  return (q, vs, ts)
 nt_patt _ _           = mzero
 
 -- parse an "of"-notion: follow the pattern to the notion name, then check that
 -- "of" follows the name followed by a variable that is not followed by "and"
 of_patt p (Wd l : ls) = wd_tokenOf l >> of_patt p ls
-of_patt p (Nm : Wd l : Vr : ls)
-                      = do  guard $ elem "of" l; vs <- namlist
-                            (q, ts) <- na_patt p ls
-                            return (q, vs, ts)
+of_patt p (Nm : Wd l : Vr : ls) = do
+  guard $ elem "of" l; vs <- namlist
+  (q, ts) <- na_patt p ls
+  return (q, vs, ts)
 of_patt _ _           = mzero
 
 -- parse a "common"-notion: basically like the above. We use the special parser
 -- s for the first variable place after the "of" since we expect multiple terms
 -- here. Example: A common *divisor of m and n*.
 cm_patt p s (Wd l:ls) = wd_tokenOf l >> cm_patt p s ls
-cm_patt p s (Nm : Wd l : Vr : ls)
-                      = do  guard $ elem "of" l; vs <- namlist; wd_tokenOf l
-                            (r, as) <- s; when (null $ tail as) $
-                              fail "several objects expected for `common'"
-                            (q, ts) <- na_patt p ls
-                            return (r . q, vs, as, ts)
+cm_patt p s (Nm : Wd l : Vr : ls) = do
+  guard $ elem "of" l; vs <- namlist; wd_tokenOf l
+  (r, as) <- s
+  when (null $ tail as) $ fail "several objects expected for `common'"
+  (q, ts) <- na_patt p ls
+  return (r . q, vs, as, ts)
 cm_patt _ _ _         = mzero
 
 -- an auxiliary pattern parser that checks that we are not dealing wiht an "and"
 -- wd_token and then continues to follow the pattern
-na_patt p (Wd l : ls) = guard (not $ elem "and" l) >> wd_tokenOf l >> wd_patt p ls
+na_patt p (Wd l : ls) = guard (notElem "and" l) >> wd_tokenOf l >> wd_patt p ls
 na_patt p ls          = wd_patt p ls
 
 
@@ -248,18 +261,21 @@ na_patt p ls          = wd_patt p ls
 
 namlist = varlist -|- liftM (:[]) hidden
 
-varlist = do  vs <- var `sepBy` wd_token ","
-              nodups vs ; return vs
+varlist = do
+  vs <- var `sepBy` wd_token ","
+  nodups vs ; return vs
 
 nodups vs = unless ((null :: [b] -> Bool) $ duplicateNames vs) $
-              fail $ "duplicate names: " ++ show vs
+  fail $ "duplicate names: " ++ show vs
 
-hidden  = do n <- MS.gets hid_count
-             MS.modify $ \st -> st {hid_count = succ n}
-             return ('h':show n)
+hidden = do
+  n <- MS.gets hid_count
+  MS.modify $ \st -> st {hid_count = succ n}
+  return ('h':show n)
 
-var     = do v <- satisfy (\s -> all isAlphaNum s && isAlpha (head s))
-             return ('x':v)
+var = do
+  v <- satisfy (\s -> all isAlphaNum s && isAlpha (head s))
+  return ('x':v)
 
 --- pretyped Variables
 
@@ -268,9 +284,10 @@ type TVar = ([String], Formula)
 prim_tvr :: FTL MNotion
 prim_tvr  = getExpr tvr_expr tvr
   where
-    tvr (vr, nt)  = do  vs <- varlist
-                        guard $ all (`elem` vr) vs
-                        return (id, nt, vs)
+    tvr (vr, nt)  = do
+      vs <- varlist
+      guard $ all (`elem` vr) vs
+      return (id, nt, vs)
 
 -- free
 
@@ -296,10 +313,10 @@ decl vs = dive
 
 overfree :: [String] -> Formula -> Maybe String
 overfree vs f
-    | occurs zSlot f  = Just $ "too few subjects for an m-predicate " ++ inf
-    | not (null sbs)  = Just $ "free undeclared variables: "   ++ sbs ++ inf
-    | not (null ovl)  = Just $ "overlapped variables: "        ++ ovl ++ inf
-    | otherwise       = Nothing
+    | occurs zSlot f = Just $ "too few subjects for an m-predicate " ++ inf
+    | not (null sbs) = Just $ "free undeclared variables: "   ++ sbs ++ inf
+    | not (null ovl) = Just $ "overlapped variables: "        ++ ovl ++ inf
+    | otherwise      = Nothing
   where
     sbs = unwords $ map showVar $ free vs f
     ovl = unwords $ map showVar $ over vs f
@@ -309,9 +326,10 @@ overfree vs f
     over vs (Exi v f) = bvrs vs v f
     over vs f = foldF (over vs) f
 
-    bvrs vs v f | elem v vs = [v]
-                | null v    = over vs f
-                | otherwise = over (v:vs) f
+    bvrs vs v f
+      | elem v vs = [v]
+      | null v    = over vs f
+      | otherwise = over (v:vs) f
 
 
 --- macro expressions
