naproche/naproche

commit 7c8337eedd79a2013ac0d28be7b038c46447fa58

Use 'nubOrd' where possible + comments This leads to minor speed-ups

src/SAD/Prove/Normalize.hs
@@ -195,6 +195,7 @@ distrib :: [[Formula]] -> [[Formula]] -> [[Formula]]
 distrib ls rs = nubBy listEq $ allpairs (unionBy ltTwins) ls rs
 
 
+{-| Does every formula from the first list have an 'ltTwins' in the second? -}
 listEq :: [Formula] -> [Formula] -> Bool
 listEq [] [] = True
 listEq (l:ls) rs = case helper [] l rs of
@@ -204,7 +205,6 @@ listEq (l:ls) rs = case helper [] l rs of
     helper _ _ [] = Nothing
     helper dmp l (r:rs) = if ltTwins l r then return $ dmp ++ rs
                           else helper (r:dmp) l rs
-listEq _ _ = False
 
 trivial :: [Formula] -> Bool
 trivial [] = False

.travis.yml
@@ -2,6 +2,8 @@
 # of eprover and Isabelle. It is based on the simple stack configuration, see:
 #
 # https://docs.haskellstack.org/en/stable/travis_ci/
+#
+# TODO: This returns success even if some (or all) of the verifications failed!
 
 # Choose a build environment
 dist: xenial

src/SAD/Data/Formula/Base.hs
@@ -26,6 +26,7 @@ data Formula =
         trInfo :: [Formula], trId   :: Int}         |
   Var { trName :: String   , trInfo :: [Formula], trPosition :: SourcePos } |
   Ind { trIndx :: Int, trPosition :: SourcePos }   | ThisT
+  deriving (Eq, Ord)
 
 
 -- Traversing functions
@@ -135,13 +136,13 @@ foldF fn (Not f) = fn f
 foldF fn t@Trm{} = Monoid.mconcat $ map fn $ trArgs t
 foldF _ _ = Monoid.mempty
 
-{- tests whether a predicate holds for all formulas on the next structure level
-   of a formula -}
+{- | tests whether a predicate holds for all formulas on the next structure level
+   of a formula. Returns 'True' if there is none. -}
 allF :: (Formula -> Bool) -> Formula -> Bool
 allF predicate = Monoid.getAll . foldF (Monoid.All . predicate)
 
-{- tests whether a predicate holds for any formula on the next structure level
-   of a formula -}
+{- | tests whether a predicate holds for any formula on the next structure level
+   of a formula. Returns 'False' if there is none. -}
 anyF :: (Formula -> Bool) -> Formula -> Bool
 anyF predicate = Monoid.getAny . foldF (Monoid.Any . predicate)
 
@@ -178,7 +179,7 @@ closed  = dive 0
     dive n Ind {trIndx = v} = v < n
     dive n f = allF (dive n) f
 
-{- checks whether the formula t occurs anywhere in the formula f -}
+{- checks whether the non-compound formula t occurs anywhere in the formula f -}
 occurs :: Formula -> Formula -> Bool
 occurs t f = twins t f || anyF (occurs t) f
 

src/SAD/Data/Formula/Kit.hs
@@ -10,16 +10,19 @@ import Control.Monad
 import Data.Maybe
 import qualified Data.Map as M
 import Data.List
+import Data.Function (on)
 
 import SAD.Data.Formula.Base
 import SAD.Data.Tag
 import SAD.Core.SourcePos
 import SAD.Data.Text.Decl (Decl)
 import qualified SAD.Data.Text.Decl as Decl
+import SAD.Helpers
 
 -- Alpha-beta normalization
 
--- | gets rid of top level negation, implication and equivalence
+-- | gets rid of top level negation, implication and equivalence if possible.
+-- @albet (Not (Ind ..)) == Not (Ind ..)@
 albet :: Formula -> Formula
 albet (Iff f g)       = zIff f g
 albet (Imp f g)       = Or (Not f) g
@@ -213,29 +216,17 @@ zObj      = zTrm objectId "aObj" . pure -- this is a dummy for parsing purposes
 
 -- predefined identifier
 
-equalityId :: Int
 equalityId    =  -1 :: Int
-lessId :: Int
 lessId        =  -2 :: Int
-thesisId :: Int
 thesisId      =  -3 :: Int
-functionId :: Int
 functionId    =  -4 :: Int
-applicationId :: Int
 applicationId =  -5 :: Int
-domainId :: Int
 domainId      =  -6 :: Int
-setId :: Int
 setId         =  -7 :: Int
-elementId :: Int
 elementId     =  -8 :: Int
-productId :: Int
 productId     =  -9 :: Int
-pairId :: Int
 pairId        = -10 :: Int
-objectId :: Int
 objectId      = -11 :: Int
-newId :: Int
 newId         = -15 :: Int -- temporary id given to newly introduced symbols
 
 
@@ -366,7 +357,7 @@ match _ _ = mzero
 
 -- Misc stuff
 
-{- strip away a Tag on top level or after neation -}
+{- strip away a Tag on top level or after negation -}
 strip :: Formula -> Formula
 strip (Tag _ f) = strip f
 strip (Not f)   = Not $ strip f
@@ -393,7 +384,7 @@ tryToGetID _ = mzero
 {- return free user named variables in a formula (without duplicateNames),
 except those in vs -}
 freePositions :: [String] -> Formula -> [(String, SourcePos)]
-freePositions vs = nubBy (\a b -> fst a == fst b) . dive
+freePositions vs = nubOrdBy (compare `on` fst) . dive
   where
     dive f@Var {trName = u@('x':_)} = 
       (guard (u `notElem` vs) >> return (u, trPosition f)) ++ foldF dive f
@@ -405,7 +396,7 @@ free vs = map fst . freePositions vs
 {- return all free variables in a formula (without duplicateNames),
 except those in vs -}
 allFree :: [String] -> Formula -> [String]
-allFree vs = nub . dive
+allFree vs = nubOrd . dive
   where
     dive f@Var {trName = u} = guardNotElem vs u ++ foldF dive f
     dive f = foldF dive f

src/SAD/Data/Tag.hs
@@ -7,7 +7,7 @@ data Tag =
   GenericMark | Evaluation | Condition | Defined | Domain | Replacement |
   -- Tags to mark parts in function proof tasks
   DomainTask | ExistenceTask | UniquenessTask | ChoiceTask
-  deriving (Eq, Show)
+  deriving (Eq, Ord, Show)
 
 {- whether a Tag marks a part in a function proof task -}
 fnTag :: Tag -> Bool

src/SAD/Data/Text/Decl.hs
@@ -19,7 +19,7 @@ data Decl = Decl {
   name :: String,
   position :: SourcePos,
   serial :: Serial
-}
+} deriving (Eq, Ord)
 
 {- a declaration that has no representation in the input text -}
 nonText :: String -> Decl

src/SAD/Export/DFG.hs
@@ -13,6 +13,7 @@ import SAD.Data.Text.Block (Block(Block))
 import  qualified SAD.Data.Text.Block as Block
 import SAD.Data.Text.Context as Context (Context(..))
 import SAD.Export.Base
+import SAD.Helpers
 
 output :: Bool -> Prover -> Int -> [Context] -> Context -> String
 output red _ _ cn gl = (hdr . sym . axm . cnj . eop) ""
@@ -81,7 +82,7 @@ dfgSLS tsk  = sls "functions" fns . sls "predicates" pds
 
     pds = [ (s, a) | (True,  s, a) <- sms ]
     fns = [ (s, a) | (False, s, a) <- sms ]
-    sms = foldr (union . nub . dfgSyms True . Context.formula) [] tsk
+    sms = foldr (union . nubOrd . dfgSyms True . Context.formula) [] tsk
 
 dfgSyms :: Bool -> Formula -> [(Bool, String, Int)]
 dfgSyms s f | isEquality f   = concatMap (dfgSyms False) $ trArgs f

src/SAD/ForTheL/Reports.hs
@@ -10,6 +10,7 @@ module SAD.ForTheL.Reports where
 
 import qualified Control.Monad.State.Class as MS
 import Data.List
+import SAD.Helpers (nubOrd)
 
 import SAD.Core.Message (PIDE)
 import qualified SAD.Core.Message as Message
@@ -56,7 +57,7 @@ variableReport pide def decl pos =
     _ -> []
 
 formulaReports :: PIDE -> [Decl] -> Formula -> [Message.Report]
-formulaReports pide decls = nub . dive
+formulaReports pide decls = nubOrd . dive
   where
     dive Var {trName = name, trPosition = pos} =
       (pos, Markup.free) : entity

commit 997b36468106d0ca5f764d6be8486ca05b93e7ea

Fix error introduced in 7c8337e

src/SAD/Prove/Normalize.hs
@@ -191,6 +191,7 @@ listEq (l:ls) rs = case helper [] l rs of
     helper _ _ [] = Nothing
     helper dmp l (r:rs) = if ltTwins l r then return $ dmp ++ rs
                           else helper (r:dmp) l rs
+listEq _ _ = False
 
 allpairs :: (a -> b -> c) -> [a] -> [b] -> [c]
 allpairs f [] _ = []