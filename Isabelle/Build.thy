4ba40c58e69a40c0f2959e0ddbd848f82b27fc1a
Makarius <makarius@sketis.net>
2021-01-17 11:44:10 +0100

    More robust build of naproche.jar outside of Isabelle/Scala: a bad version could destroy the Scala class context.

--- Isabelle/Build.thy
+++ Isabelle/Build.thy
@@ -1,29 +1,15 @@
 (*
-Authors: Makarius Wenzel (2018)
+Authors: Makarius Wenzel (2021)
 
-Build Isabelle/Naproche modules.
+Build Isabelle/Naproche Haskell modules.
 *)
 
 theory Build
-  imports Pure Haskell.Haskell Naproche.Naproche
+  imports Haskell.Haskell Naproche.Naproche
 begin
 
 section \<open>Isabelle/Haskell modules\<close>
 
 export_generated_files _ (in Haskell) and _ (in Naproche)
-
-
-section \<open>Isabelle/Scala modules\<close>
-
-external_file \<open>file_format.scala\<close>
-external_file \<open>prover_server.scala\<close>
-
-compile_generated_files
-  external_files \<open>file_format.scala\<close> \<open>prover_server.scala\<close>
-  export_files \<open>naproche.jar\<close>
-  where \<open>fn dir =>
-    ignore (Generated_Files.execute dir \<open>Build\<close>
-      "isabelle scalac $ISABELLE_SCALAC_OPTIONS *.scala && \
-      \isabelle_jdk jar cf naproche.jar *")\<close>
 
 end
