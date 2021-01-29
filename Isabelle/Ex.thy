568670a7600ac4973d4a3fb812b2884751fa2071
Makarius <makarius@sketis.net>
2021-01-29 20:55:53 +0100

    Tuned.

--- Isabelle/Ex.thy
+++ Isabelle/Ex.thy
@@ -8,7 +8,7 @@ text \<open>
   Naproche-SAD is a project managed by Prof. Peter Koepke, from the
   Mathematical Institute, University of Bonn.
 
-  The aim is to check mathematical texts, written in restricted natural
+  The aim is to check mathematical texts, written in controlled natural
   language. There are two input formats:
 
     \<^enum> The Formal Theory Language (ForTheL), e.g. see
@@ -20,7 +20,8 @@ text \<open>
   Further examples are available in the directory \<^dir>\<open>$NAPROCHE_HOME/examples\<close>.
 
   The Isabelle/jEdit Prover IDE understands both \<^verbatim>\<open>.ftl\<close> and \<^verbatim>\<open>.ftl.tex\<close>
-  files, with spontaneous checking by the Naproche-SAD server process.
+  files, with spontaneous checking by the Naproche-SAD server process
+  running in the background.
 \<close>
 
 end
