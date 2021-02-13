6b43e8dc6acf75bb2d4f077942c51cd187630032
Your Name <you@example.com>
2021-02-13 23:40:41 +0100

    Rewritten Ex.thy

--- Isabelle/Ex.thy
+++ Isabelle/Ex.thy
@@ -10,22 +10,58 @@ theory Ex
   imports Naproche.Naproche
 begin
 
-section \<open>The Naproche project\<close>
+section \<open>Isabelle/Naproche\<close>
 
 text \<open>
-  Naproche is a project managed by Prof. Peter Koepke, from the Mathematical
-  Institute, University of Bonn. The aim is to check mathematical texts,
-  written in controlled natural language.
+  The Isabelle/Naproche system provides interactive editing and
+  automatic checking of mathematical texts, written in the 
+  controlled natural language ForTheL. ForTheL files in .ftl.tex format use 
+  LaTeX symbols and constructs, and may be compiled to pdf. The development 
+  of the Naproche proof checker is carried out at the University of Bonn,
+  coordinated by Peter Koepke (koepke@math.uni-bonn.de). 
+  The source code repository is 
+  \<^url>\<open>https://github.com/anfelor/Naproche-SAD\<close>
 
-  The source code repository is \<^url>\<open>https://github.com/anfelor/Naproche-SAD\<close>
+  The Naproche system
+  is part of the longterm Naproche (Natural Proof Checking) project 
+  at the Universities of Bonn and Duisburg-Essen 
+  (\<^url>\<open>http://naproche.net\<close>). Andrei Paskevich kindly let us take over the
+  source code of his System for Automated Deduction 
+  (http://nevidal.org/sad.en.html) and gave essential advice. Program
+  development was carried out by Steffen Frerix, Adrian De Lon
+  and Anton Lorenzen. Adrian Marti and Marcel Schütz contributed to the 
+  present release. The Naproche system is experimental, research quality
+  software which may exhibit unexpected behaviour and bugs.  
 \<close>
 
-
-section \<open>Examples\<close>
+section \<open>Tutorial\<close>
 
 text \<open>
-  The Isabelle/jEdit Prover IDE can check \<^verbatim>\<open>.ftl\<close> and \<^verbatim>\<open>.ftl.tex\<close> files;
-  corresponding \<^verbatim>\<open>.pdf\<close> files have been produced by regular \<^verbatim>\<open>pdflatex\<close>.
+  A brief introduction to Naproche is contained in
+
+    \<^enum> \<^file>\<open>$NAPROCHE_HOME/examples/TUTORIAL.ftl.pdf\<close>.
+
+  Practical formalization experiments can
+  carried out by playing with the source code of this tutorial which
+  itself is a proof-checked ForTheL text:
+
+    \<^enum> \<^file>\<open>$NAPROCHE_HOME/examples/TUTORIAL.ftl.tex\<close>.
+
+  Andrei Paskevich's "The syntax and semantics of the ForTheL language"
+
+    \<^enum> \<^url>\<open>http://nevidal.org/download/forthel.pdf\<close> 
+
+  is still the best in-depth guide to ForTheL.
+\<close>
+section \<open>Examples\<close>
+
+text \<open>The folder \<^dir>\<open>$NAPROCHE_HOME/examples\<close> contains a selection of
+  formalizations intended to demonstrate the naturalness and
+  coverage of formalizations in Naproche. Some of the examples
+  have been taken from SAD and rewritten in the LaTeX dialect of
+  ForTheL. The Isabelle/jEdit Prover IDE can check \<^verbatim>\<open>.ftl\<close> and 
+  \<^verbatim>\<open>.ftl.tex\<close> files; corresponding \<^verbatim>\<open>.pdf\<close> files have been produced 
+  by regular \<^verbatim>\<open>pdflatex\<close>.
 
   For example:
 
@@ -33,22 +69,9 @@ text \<open>
                      
     \<^enum> \<^file>\<open>$NAPROCHE_HOME/examples/tarski.ftl.tex\<close>
 
-    \<^enum> \<^file>\<open>$NAPROCHE_HOME/examples/tarski.ftl.pdf\<close>
+    \<^enum> \<^file>\<open>~~/naproche-069986ef7f3a/examples/tarski.ftl.pdf\<close>
 
-  More examples: \<^dir>\<open>$NAPROCHE_HOME/examples\<close>
 \<close>
-
-
-section \<open>Tutorial\<close>
-
-text \<open>
-  See:
-
-    \<^enum> \<^file>\<open>$NAPROCHE_HOME/examples/TUTORIAL.ftl.tex\<close>
-
-    \<^enum> \<^file>\<open>$NAPROCHE_HOME/examples/TUTORIAL.ftl.pdf\<close>
-\<close>
-
 
 section \<open>Implementation and system integration\<close>
 
