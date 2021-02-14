fecbe1f7ec0d26a770a9b6eb2e77f34d0ea974a4
Makarius <makarius@sketis.net>
2021-02-14 16:14:46 +0100

    Proper PATH for Isabelle provers.

--- Isabelle/naproche_test.scala
+++ Isabelle/naproche_test.scala
@@ -51,7 +51,9 @@ object Naproche_Test
             def check_timeout: Boolean =
               Time.now() > start + timeout && { was_timeout = true; true }
             val result =
-              Isabelle_System.bash("\"$NAPROCHE_EXE\" -- " + File.bash_path(path),
+              Isabelle_System.bash(
+                "export PATH=\"$E_HOME:$SPASS_HOME:$PATH\"\n" +
+                "\"$NAPROCHE_EXE\" -- " + File.bash_path(path),
                 cwd = naproche_home.file,
                 strict = false,
                 watchdog =
