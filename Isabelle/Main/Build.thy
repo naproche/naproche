536469a2b842f259ebc6af514e406643a1334ea6
Makarius <makarius@sketis.net>
2021-07-25 21:48:50 +0200

    Tuned comments.

--- Isabelle/Main/Build.thy
+++ Isabelle/Main/Build.thy
@@ -1,14 +1,12 @@
 (*
 Authors: Makarius Wenzel (2021)
 
-Build Isabelle/Naproche Haskell modules.
+Export Isabelle/Naproche Haskell modules.
 *)
 
 theory Build
   imports Haskell.Haskell Naproche.Naproche
 begin
-
-section \<open>Isabelle/Haskell modules\<close>
 
 export_generated_files _ (in Haskell) and _ (in Naproche)
 
