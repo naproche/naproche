7a336b65cef8a5fc9e6fb0df9c46d30a14dad997
Makarius <makarius@sketis.net>
2021-11-28 18:58:10 +0100

    Prefer examples that actually work.

--- Isabelle/Test/Test.thy
+++ Isabelle/Test/Test.thy
@@ -15,38 +15,65 @@ subsection \<open>Inlined text\<close>
 declare [[naproche_isabelle]]
 
 forthel \<open>
-[synonym subset/-s] [synonym surject/-s]
+[synonym element/-s] [synonym system/-s] [synonym reduct/-s] [synonym term/-s]
 
-Let M denote a set.
-Axiom. M is setsized.
-Let f denote a function.
-Let the value of f at x stand for f(x).
-Let f is defined on M stand for Dom(f) = M.
-Let the domain of f stand for Dom(f).
+Signature Rewriting system.  A rewriting system is a notion.
+Signature Term. A term is an object.
 
-Axiom. Let x be an element of M. x is setsized.
-Axiom. The value of f at any element of the domain of f is a set.
+Let a,b,c,d,u,v,w,x,y,z denote terms.
+Let R,S,T denote rewriting systems.
 
-Definition.
-A subset of M is a set N such that every element of N is an element of M.
+Signature Reduct.   A reduct of x in R is a term.
 
-Definition.
-The powerset of M is the class of subsets of M.
+Let x -R> y stand for y is a reduct of x in R.
 
-Axiom. The powerset of M is a set.
+Signature. x -R+> y is a relation.
 
-Definition.
-f surjects onto M iff every element of M is equal to the value of f at some element of the domain of f.
+Axiom TCDef.   x -R+> y <=> x -R> y \/ exists z : x -R> z -R+> y.
+Axiom TCTrans.      x -R+> y -R+> z => x -R+> z.
 
-Proposition.
-No function that is defined on M surjects onto the powerset of M.
-Proof by contradiction.
-Assume the contrary. Take a function f that is defined on M and surjects onto the powerset of M.
-Define N = { x in M | x is not an element of f(x) }.
-Then N is not equal to the value of f at any element of M.
-Contradiction. qed.
+Definition TCRDef.  x -R*> y <=> x = y \/ x -R+> y.
+Lemma TCRTrans.     x -R*> y -R*> z => x -R*> z.
 
-Theorem. The value of f at any element of the domain of f is a set.
+Definition CRDef.   R is confluent iff
+    for all a,b,c  such that a -R*> b,c
+    there exists d such that b,c -R*> d.
+
+Definition WCRDef.  R is locally confluent iff
+    for all a,b,c  such that a -R> b,c
+    there exists d such that b,c -R*> d.
+
+Definition Termin.  R is terminating iff for all a,b
+    a -R+> b => b -<- a.
+
+Definition NFRDef.  A normal form of x in R is a term y
+                    such that x -R*> y and y has no reducts in R.
+
+Lemma TermNF.   Let R be a terminating rewriting system.
+                Every term x has a normal form in R.
+Proof by induction. Obvious.
+
+Lemma Newman.
+    Any locally confluent terminating rewriting system is confluent.
+Proof.
+Let R be a rewriting system.
+    Assume R is locally confluent and terminating.
+    Let us demonstrate by induction that for all a,b,c
+    such that a -R*> b,c there exists d such that b,c -R*> d.
+        Let a,b,c be terms.
+        Let us show that if a -R+> b,c then thesis.
+            Assume a -R+> b,c.
+
+            Take u such that a -R> u -R*> b.
+            Take v such that a -R> v -R*> c.
+            Take w such that u,v -R*> w.
+            Take a normal form d of w in R.
+    
+            b -R*> d. Indeed take x such that b,d -R*> x.
+            c -R*> d. Indeed take y such that c,d -R*> y.
+        End.
+    End.
+End.
 \<close>
 
 
@@ -55,8 +82,8 @@ subsection \<open>External text file\<close>
 context notes [[naproche_prove = false]]
 begin
 
-forthel_file \<open>$NAPROCHE_HOME/examples/cantor.ftl\<close>
-forthel_file \<open>$NAPROCHE_HOME/examples/cantor.ftl.tex\<close>
+forthel_file \<open>$NAPROCHE_HOME/examples/newman.ftl\<close>
+forthel_file \<open>$NAPROCHE_HOME/examples/newman.ftl.tex\<close>
 
 end
 
