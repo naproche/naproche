978204a4910f8b95e8281fb630adad925238a3ff
mcearl <ms@marcelschuetz.de>
2024-02-29 19:21:16 +0100

    Clean up FTL tokenizer

--- src/SAD/Tokenizer/FTL.hs
+++ src/SAD/Tokenizer/FTL.hs
@@ -15,46 +15,53 @@ import SAD.Core.Message qualified as Message
 import Isabelle.Markup qualified as Markup
 
 
--- * Tokenizing FTL input
-
--- | Remove all comments from a list of tokens.
 tokenizeFtl :: [Lexeme] -> [Token.Token]
-tokenizeFtl [] = []
-tokenizeFtl (lexeme:rest) = case lexemeToToken lexeme of
-  Nothing -> tokenizeFtl rest
-  Just token -> token : tokenizeFtl rest
-
--- | Essentially the same as "filterFtl" to be used inside a PIDE.
-tokenizeFtlPIDE :: [Lexeme] -> IO [Token.Token]
-tokenizeFtlPIDE [] = pure []
-tokenizeFtlPIDE (lexeme:rest) = do
-  mbToken <- lexemeToTokenPIDE lexeme
-  case mbToken of
-    Nothing -> tokenizeFtlPIDE rest
-    Just token -> fmap (token :) (tokenizeFtlPIDE rest)
+tokenizeFtl = concatMap lexemeToToken
 
 -- | Transform a lexeme into a @Maybe@-wrapped token: If the lexeme is a comment,
 -- @Nothing@ is returned; otherwise @Just token@ is returned for an appropriate
 -- token @token@.
-lexemeToToken :: Lexeme -> Maybe Token.Token
-lexemeToToken (Comment _ pos) = Nothing
-lexemeToToken (EOF pos) = Just $ Token.EOF pos
-lexemeToToken (Symbol char pos ws) = Just $ Token.Token {
-    Token.tokenText = Text.singleton char,
-    Token.tokenPos = pos,
-    Token.tokenType = if ws then Token.WhiteSpaceBefore else Token.NoWhiteSpaceBefore
-  }
-lexemeToToken (Lexeme text pos ws) = Just $ Token.Token {
-    Token.tokenText = Text.pack text,
-    Token.tokenPos = pos,
-    Token.tokenType = if ws then Token.WhiteSpaceBefore else Token.NoWhiteSpaceBefore
-  }
+lexemeToToken :: Lexeme -> [Token.Token]
+-- Comment:
+lexemeToToken (Comment _ pos) = []
+-- EOF:
+lexemeToToken (EOF pos) =
+  let token = Token.EOF pos
+  in [token]
+-- Symbol:
+lexemeToToken (Symbol char pos ws) =
+  let token = Token.Token {
+        Token.tokenText = Text.singleton char,
+        Token.tokenPos = pos,
+        Token.tokenType = if ws then Token.WhiteSpaceBefore else Token.NoWhiteSpaceBefore
+      }
+  in [token]
+-- Lexeme:
+lexemeToToken (Lexeme text pos ws) =
+  let token = Token.Token {
+        Token.tokenText = Text.pack text,
+        Token.tokenPos = pos,
+        Token.tokenType = if ws then Token.WhiteSpaceBefore else Token.NoWhiteSpaceBefore
+      }
+  in [token]
+
+
+tokenizeFtlPIDE :: [Lexeme] -> IO [Token.Token]
+tokenizeFtlPIDE [] = pure []
+tokenizeFtlPIDE (l : ls) = do
+  fstTok <- lexemeToTokenPIDE l
+  restToks <- tokenizeFtlPIDE ls
+  return $ fstTok ++ restToks
 
 -- | Essentially the same as "lexemeToToken" to be used inside a PIDE.
-lexemeToTokenPIDE :: Lexeme -> IO (Maybe Token.Token)
+lexemeToTokenPIDE :: Lexeme -> IO [Token.Token]
+-- Comment:
 lexemeToTokenPIDE (Comment _ pos) = do
   Message.reports [(pos, Markup.comment1)]
-  return Nothing
-lexemeToTokenPIDE token@(EOF pos) = pure $ lexemeToToken token
-lexemeToTokenPIDE token@(Symbol char pos ws) = pure $ lexemeToToken token
-lexemeToTokenPIDE token@(Lexeme text pos ws) = pure $ lexemeToToken token
+  return []
+-- EOF:
+lexemeToTokenPIDE token@EOF{} = pure $ lexemeToToken token
+-- Symbol:
+lexemeToTokenPIDE token@Symbol{} = pure $ lexemeToToken token
+-- Lexeme:
+lexemeToTokenPIDE token@Lexeme{} = pure $ lexemeToToken token
